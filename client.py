ad969ae9530caef5d5a4ad9c7d2fae3eebcc4f0e
Caio <caioroberto@discente.ufg.br>
2023-05-18 13:35:12 -0300

    Commit inicial - mercado

--- client.py
+++ client.py
@@ -1,9 +1,10 @@
 from socket  import *
 from constCS import * #-
+import pickle
 
 s = socket(AF_INET, SOCK_STREAM)
 s.connect((HOST, PORT)) # connect to server (block until accepted)
-s.send(str.encode('Hello, world'))  # send some data
+s.send(pickle.dumps([['banana', 3], ['abacaxi', 4]]))  # send some data
 data = s.recv(1024)     # receive the response
-print (bytes.decode(data))            # print the result
+print (pickle.loads(data))            # print the result
 s.close()               # close the connection
--- server.py
+++ server.py
@@ -1,13 +1,27 @@
 from socket  import *
 from constCS import * #-
+import pickle
 
 s = socket(AF_INET, SOCK_STREAM) 
 s.bind((HOST, PORT))  #-
 s.listen(1)           #-
 (conn, addr) = s.accept()  # returns new socket and addr. client 
+
 while True:                # forever
-  data = conn.recv(1024)   # receive data from client
-  if not data: break       # stop if client stopped
-  print(bytes.decode(data))
-  conn.send(str.encode(bytes.decode(data)+"*")) # return sent data plus an "*"
+  msg = conn.recv(1024)   # receive data from client
+  if not msg: break       # stop if client stopped
+  data = pickle.loads(msg)
+  total_value = 0
+  str_back = ''
+  for element in data:
+    if element[0] == 'laranja':
+      total_value += 3*int(element[1])
+    elif element[0] == 'banana':
+      total_value += 4*int(element[1])
+    elif element[0] == 'abacaxi':
+      total_value += 5*int(element[1])
+    else:
+      str_back = element[0] + 'not in stock'
+  str_back = '\n soma total: ' + str(total_value)
+  conn.send(pickle.dumps(str_back)) # return the total value of the buy
 conn.close()               # close the connection
